253981237240c56709918544cdc383b21e06c76f
Icon Jen <iconjen@gmail.com>
2016-08-31 14:13:32 +1000

    created models

---
+++ app/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'user_id', 'active', 'title', 'body'
+    ];
+    
+    protected $dates = [
+        'deleted_at', 'published_at'
+    ];
+    
+    public function Author() {
+        return $this->belongsTo('User');
+    }
+}
--- app/User.php
+++ app/User.php
@@ -23,4 +23,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    protected $dates = ['deleted_at'];
+    
+    public function Posts() {
+        return $this->hasMany('Post');
+    }
 }
