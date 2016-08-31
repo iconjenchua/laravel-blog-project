78b3cbf3dd4bc1824aec8ce1d53bbc230b8e4bcc
Icon Jen <iconjen@gmail.com>
2016-08-31 22:59:15 +1000

    created form request for editing posts

---
+++ app/Http/Requests/EditPost.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class EditPost extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if(\Auth::check()) return true;
+        
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'user_id' => 'required|exists:users,id',
+            'title' => 'required',
+            'active' => 'required',
+            'body' => 'required'
+        ];
+    }
+}
