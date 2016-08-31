fb10781837358c487e409cd72c07ee861d77da0a
Icon Jen <iconjen@gmail.com>
2016-08-31 14:10:00 +1000

    added migrations

--- database/migrations/2016_08_31_140310_create_post_table.php
+++ database/migrations/2016_08_31_140310_create_post_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePostTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,14 +12,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function(Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('user_id')->unsigned();
+            $table->boolean('active');
+            $table->string('title', 64);
+            $table->text('body');
             $table->timestamps();
-            
+            $table->timestamp('published_at');
             $table->softDeletes();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('posts');
     }
 }
