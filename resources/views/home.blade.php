64b3e8e7a6c70157cbca8ae1264ddb65db4c4b01
Icon Jen <iconjen@gmail.com>
2016-08-31 19:05:49 +1000

    new views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('main')
+
+@section('content')
+@foreach($posts as $post)
+<div class="blog-post">
+    <h2 class="blog-post-title">{{ $post->title }}</h2>
+    <p class="blog-post-meta">{{ $post->published_at->format('F j, Y H:i A') . ' by ' . $post->Author->name }}</p>
+    {{ Illuminate\Support\Str::words($post->body, 20) }}
+    <p><a href="{{ route('post', [$post->id, str_slug($post->title)]) }}">Read more &raquo;</a></p>
+</div>
+@endforeach
+
+<div class="pagination">
+    {{ $posts->links() }}
+</div>
+@endsection
---
+++ resources/views/post.blade.php
@@ -0,0 +1,24 @@
+@extends('main')
+
+@section('content')
+<div class="blog-post">
+    <h2 class="blog-post-title">{{ $post->title }}</h2>
+    <p class="blog-post-meta">{{ $post->published_at->format('F j, Y H:i A') . ' by ' . $post->Author->name }}</p>
+    {{ $post->body }}
+</div>
+
+<hr>
+
+<h3>Related posts</h3>
+<div class="row">
+    @foreach($related_posts as $post)
+    <div class="col-lg-4 related-post">
+        <h4>{{ $post->title }}</h4>
+        <div class="excerpt">
+            {{ Illuminate\Support\Str::words($post->body, 20) }}
+            <p><a href="{{ route('post', [$post->id, str_slug($post->title)]) }}">Read more &raquo;</a></p>
+        </div>
+    </div>
+    @endforeach
+</div>
+@endsection
