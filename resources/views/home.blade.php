750aa56b3beb01d60642f0dbaed3c90103859fc1
Icon Jen <iconjen@gmail.com>
2016-08-31 21:31:41 +1000

    placed the post link on the post title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,14 @@
 @extends('main')
 
 @section('content')
+@if(count($posts))
 @foreach($posts as $post)
 <div class="blog-post">
-    <h2 class="blog-post-title">{{ $post->title }}</h2>
+    <h2 class="blog-post-title">
+        <a href="{{ route('post', [$post->id, str_slug($post->title)]) }}">
+            {{ $post->title }}
+        </a>
+    </h2>
     <p class="blog-post-meta">
         {{ $post->published_at->format('F j, Y H:i A') . ' by ' . $post->Author->name }}
         @if(Auth::check())(<a href="{{ route('post.edit', $post->id) }}">Edit</a>)@endif
@@ -12,7 +17,9 @@
     <p><a href="{{ route('post', [$post->id, str_slug($post->title)]) }}">Read more &raquo;</a></p>
 </div>
 @endforeach
-
+@else
+<div class="alert alert-primary"><h3>No posts found</h3></div>
+@endif
 <div class="pagination">
     {{ $posts->links() }}
 </div>
