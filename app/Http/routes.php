cb1bad597b336d1071a6df623ce2f9786563d055
Icon Jen <iconjen@gmail.com>
2016-08-31 15:39:05 +1000

    updated main route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['web']], function() {
+    
+    Route::get('/', 'BlogController@homepage');
 });
