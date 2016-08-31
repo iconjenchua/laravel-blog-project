c5cace29ee5edfa82049f06355df53bc4f4f0ded
Icon Jen <iconjen@gmail.com>
2016-08-31 23:25:53 +1000

    added create new post method

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,24 +11,45 @@ use App\Post;
 use App\User;
 
 class AdminController extends Controller
-{
-    public function postLogin(UserLogin $request) {
-        
-        if(\Auth::attempt(['email' => $request->get('email'), 'password' => $request->get('password')])) {
-            return 'okay!';
-        }
-        
-        return redirect()->back()->withInput()->withErrors('Invalid email/password.');
-    }
-    
+{   
     public function dashboard() {
         
-        $posts = Post::with('Author')->paginate(10);
+        $posts = Post::with('Author')->orderBy('created_at', 'DESC')->paginate(10);
         
         $this->data['page_title'] = 'Admin Dashboard' . PAGE_TITLE;
         $this->data['posts'] = $posts;
         
         return view('admin.dashboard', $this->data);
+    }
+    
+    public function postLogin(UserLogin $request) {
+        
+        if(\Auth::attempt(['email' => $request->get('email'), 'password' => $request->get('password')])) {
+            return redirect()->route('admin');
+        }
+        
+        return redirect()->back()->withInput()->withErrors('Invalid email/password.');
+    }
+    
+    public function createPost() {
+        $users = User::lists('name', 'id');
+        
+        $this->data['page_title'] = 'Create new post: ' . PAGE_TITLE;
+        $this->data['users'] = $users;
+        
+        return view('posts.create', $this->data);
+    }
+    
+    public function storePost(EditPost $request) {
+        $post = Post::create($request->all());
+        $post->published_at = date('Y-m-d H:i:s');
+        $post->save();
+        
+        if($post->id) {
+            return redirect()->route('post.edit', $post->id)->withInfo('New post created');
+        }
+        
+        return redirect()->back()->withError('There was something wrong when trying to save your new post. Please try again.')->withInput();
     }
     
     public function editPost($id) {
@@ -42,10 +63,16 @@ class AdminController extends Controller
         return view('posts.edit', $this->data);
     }
     
-    public function savePost($id, EditPost $request) {
+    public function updatePost($id, EditPost $request) {
         
         $post = Post::whereId($id)->update($request->except('_token'));
         
         return redirect()->route('post.edit', $id)->withInfo('Post updated');
+    }
+    
+    public function logout() {
+        \Auth::logout();
+        
+        return redirect()->route('home');
     }
 }
