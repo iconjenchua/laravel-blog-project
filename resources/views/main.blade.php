e832000626851ae40a77b1242c67d5801ef3d3c7
Icon Jen <iconjen@gmail.com>
2016-08-31 19:05:39 +1000

    implemented parent-child views

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,16 +5,42 @@
 
         <!-- scripts -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-        
+
         <!-- styles  -->
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        
+        <link href="{{ url('assets/css/blog.css') }}" rel="stylesheet">
+
     </head>
     <body>
+        <div class="blog-masthead">
+            <div class="container">
+                <nav class="blog-nav">
+                    <a class="blog-nav-item active" href="#">Home</a>
+                    <a class="blog-nav-item" href="#">New features</a>
+                    <a class="blog-nav-item" href="#">Press</a>
+                    <a class="blog-nav-item" href="#">New hires</a>
+                    <a class="blog-nav-item" href="#">About</a>
+                </nav>
+            </div>
+        </div>
+
         <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+
+            <div class="blog-header">
+                <h1 class="blog-title">Laravel Blog Project</h1>
+                <p class="lead blog-description">Just a simple blog running on Laravel 5.2</p>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-8 blog-main">
+                    @yield('content')
+                </div>
+                <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+                    <div class="sidebar-module sidebar-module-inset">
+                        <p><a href="{{ route('login') }}">Login</a></p>
+                    </div>
+                </div>
             </div>
         </div>
     </body>
